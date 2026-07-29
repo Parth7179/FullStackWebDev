646bfc05deda4ef390349b76a172d77cc2491cc9
parth7179 <parthpatel71791@gmail.com>
2026-07-29 18:16:39 +0530

    Exercise 3.20

--- Part3/Phonebook/models/person.js
+++ Part3/Phonebook/models/person.js
@@ -20,6 +20,13 @@ const phoneBookSchema = mongoose.Schema({
     number:{
         type : String,
         required: true,
+        minlength:8,
+        validate: {
+            validator: function(v){
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number`
+        }
     },
 })
 
